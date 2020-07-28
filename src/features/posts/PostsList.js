b74f95365cbe2aa3899e6b21dc358e7e640724a4
nabdelqader <nassim.abdelqader.prestataire@axionesi.net>
2020-07-28 18:07:51 +0200

    first commit

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+export const PostList = () => {
+    const posts = useSelector(state => state.posts)
+
+    const renderedPosts = posts.map(post => (
+        <article className="post-excerpt">
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0,100)}</p>
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                View post
+            </Link>
+        </article>
+    ))
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {renderedPosts}
+        </section>
+    )
+}
